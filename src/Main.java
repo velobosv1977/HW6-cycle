d1a3139bf00caf8ad655807a9ec808ee1001b7ae
velobosv1977 <velobosv33@gmail.com>
2024-11-17 16:08:33 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    //Task 1.
+        System.out.println("Задача 1. Целые от 1 до 10");
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
